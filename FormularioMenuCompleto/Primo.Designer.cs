4eb6890abb067e09b2e14b2ef1ebf621d8629592
Anahis-R <nayelimarinreyes@gmail.com>
2020-11-30 19:48:44 -0500

    deber comienzo

---
+++ FormularioMenuCompleto/Primo.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace formularioMenu
+{
+    partial class Primo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbRespuesta = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbRespuesta
+            // 
+            this.lbRespuesta.AutoSize = true;
+            this.lbRespuesta.BackColor = System.Drawing.Color.Transparent;
+            this.lbRespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRespuesta.Location = new System.Drawing.Point(24, 82);
+            this.lbRespuesta.Name = "lbRespuesta";
+            this.lbRespuesta.Size = new System.Drawing.Size(0, 16);
+            this.lbRespuesta.TabIndex = 7;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(316, 31);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(114, 20);
+            this.txtNumero.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "INGRESE EL NUMERO A CALCULAR";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(462, 31);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Primo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(598, 165);
+            this.Controls.Add(this.lbRespuesta);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Primo";
+            this.Text = "Primo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbRespuesta;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ FormularioMenuCompleto/Primo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formularioMenu
+{
+    public partial class Primo : Form
+    {
+        public Primo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (this.txtNumero.Text.Length == 0)
+            {
+                MessageBox.Show("DEBES INGRESAR UN NUMERO");
+                this.txtNumero.Focus();
+                return;
+            }
+            int numero = int.Parse(this.txtNumero.Text);
+            primo(numero);
+        }
+        void primo(int numero)
+        {
+            int num = 0;
+            for (int i = 1; i <= numero; i++)
+            {
+                if ((numero % i) == 0)
+                    num = num + 1;
+            }
+            if (num > 2)
+                this.lbRespuesta.Text = "  no es Primo";
+            else
+                this.lbRespuesta.Text = "  es Primo";
+        }
+    }
+}
